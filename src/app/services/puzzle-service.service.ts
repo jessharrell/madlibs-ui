e097c7f834ff30475da51d287895e123c3a32cf9
Jess Harrell <jharrell@pillartechnology.com>
2018-05-06 08:34:47 -0400

    move puzzle service to a service folder

--- src/app/services/puzzle-service.service.ts
+++ src/app/services/puzzle-service.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import {Observable} from 'rxjs/Observable';
-import {Puzzle} from './models/puzzle';
+import {Puzzle} from '../models/puzzle';
 
 @Injectable()
 export class PuzzleService {
